71f1059bff06e71919247cdd5bfea42cc5c3ba5a
Tommy Bowden <tom@moose17.dev>
2025-02-22 13:58:48 +0000

    update unit tests to handle arranging data needed for test

--- Week2/mid-module assignment/src/tests/unittests.py
+++ Week2/mid-module assignment/src/tests/unittests.py
@@ -1,50 +1,54 @@
 import unittest
 import sys
+from sys import maxsize
 sys.path.append("../")
-from recursion.recursive_floyd import recursive_floyd_warshall, MAX_LENGTH, NO_PATH, MIN_LEVEL
+from recursion.recursive_floyd import recursive_floyd_warshall
 from iterative.iterative_floyd import iterative_floyd
+
 
 class test_recursive_floyd(unittest.TestCase):
     # This is a function that runs every time a test is executed. Like beforeAll() in jest.
     def setUp(self):
-        self.max_length = MAX_LENGTH
-        self.min_level = MIN_LEVEL
+        self.no_path = maxsize
+        
     
     def test_recursive_floyd_asserts_true_to_expected_value(self):
         # Arrange
+        min_level = 0
         test_graph = [
-            [0, 7, NO_PATH, 8],
-            [NO_PATH, 0, 5, NO_PATH],
-            [NO_PATH, NO_PATH, 0, 2],
-            [NO_PATH, NO_PATH, NO_PATH, 0]
+            [0, 7, self.no_path, 8],
+            [self.no_path, 0, 5, self.no_path],
+            [self.no_path, self.no_path, 0, 2],
+            [self.no_path, self.no_path, self.no_path, 0]
         ]
-        
         expected_output = [
             [0, 7, 12, 8],
-            [NO_PATH, 0, 5, 7],
-            [NO_PATH, NO_PATH, 0, 2],
-            [NO_PATH, NO_PATH, NO_PATH, 0]
+            [self.no_path, 0, 5, 7],
+            [self.no_path, self.no_path, 0, 2],
+            [self.no_path, self.no_path, self.no_path, 0]
         ]
+        max_length = len(test_graph[0])
         
         # Act
-        recursive_floyd_warshall(test_graph, self.min_level, self.min_level, self.min_level)
+        recursive_floyd_warshall(test_graph, min_level, min_level, min_level)
         
         #Assert
-        for i in range(self.max_length):
-            for j in range(self.max_length):
+        for i in range(max_length):
+            for j in range(max_length):
                 self.assertEqual(test_graph[i][j], expected_output[i][j])
     
     def test_recursive_floyd_and_iterative_floyd_returns_same_output(self):
-        
         # Arrange
-        graph_used_in_iterative_floyd = [
-            [0,   7,  NO_PATH, 8],
-            [NO_PATH,  0,  5,  NO_PATH],
-            [NO_PATH, NO_PATH, 0,   2],
-            [NO_PATH, NO_PATH, NO_PATH, 0]]
+        min_level = 0
+        test_graph = [
+            [0, 7, self.no_path, 8],
+            [self.no_path, 0, 5, self.no_path],
+            [self.no_path, self.no_path, 0, 2],
+            [self.no_path, self.no_path, self.no_path, 0]
+        ]
         
         # Act
-        recursive_result = recursive_floyd_warshall(graph_used_in_iterative_floyd, self.min_level, self.min_level, self.min_level)
+        recursive_result = recursive_floyd_warshall(test_graph, min_level, min_level, min_level)
         iterative_result = iterative_floyd()
 
         # Assert
