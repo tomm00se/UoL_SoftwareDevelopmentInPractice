055d8634d83c79c6e0d0fcf7b7c20ce91888539f
Tommy Bowden <tom@moose17.dev>
2025-02-25 18:19:43 +0000

    concatenate via zip

--- Week4/Activities/pythonLists.py
+++ Week4/Activities/pythonLists.py
@@ -2,3 +2,9 @@
 list1 = [100, 200, 300, 400, 500]
 result = list1.reverse()
 print(result)
+
+# 2 - Concatenate two lists index-wise
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+result = [i + j for i, j in zip(list1, list2)]
+print(result)
