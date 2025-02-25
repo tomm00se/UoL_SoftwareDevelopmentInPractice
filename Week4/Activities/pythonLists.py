2f88bd0d8273aea7f01af2b7b8927b628c3cc1d3
Tommy Bowden <tom@moose17.dev>
2025-02-25 18:25:40 +0000

    return a list of squared numbers

--- Week4/Activities/pythonLists.py
+++ Week4/Activities/pythonLists.py
@@ -8,3 +8,12 @@ list1 = ["M", "na", "i", "Ke"]
 list2 = ["y", "me", "s", "lly"]
 result = [i + j for i, j in zip(list1, list2)]
 print(result)
+
+# 3 - Turn every item of a list into its square
+numbers = [1, 2, 3, 4, 5, 6, 7]
+squaredNumbers = []
+for i in numbers:
+    squaredNumbers.append(i * i)
+
+print(squaredNumbers)
+
